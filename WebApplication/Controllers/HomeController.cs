17c6f7a011df9dfac3a112a45daa38a962866af9
fengchufu <fengchufu@163.com>
2018-12-30 23:37:31 +0800

    登录

--- WebApplication/Controllers/HomeController.cs
+++ WebApplication/Controllers/HomeController.cs
@@ -3,15 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using WebApplication.Application;
+using WebApplication.Models;
+using WebApplication.Common;
 
 namespace WebApplication.Controllers
 {
     public class HomeController : Controller
-    { 
+    {
+        private UserService userService;
+
+        public HomeController()
+        {
+            userService = new UserService();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            Models.User user = new User();
+            return View(user);
+        }
+
+        public ActionResult Login()
+        {
+            Models.User user = new User();
+            return View(user);
+        }
+
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+            var userFromDb = userService.GetUserForLoginName(user.LoginName);
+            string pwd = Utils.Encrypt(user.Password);
+            if (userFromDb == null)
+            {
+                ViewBag.Message = "用户名不存在！";
+                return View(user);
+            }
+            if (userFromDb.Password != pwd)
+            {
+                ViewBag.Message = "密码不对！";
+                return View(user);
+            }
+             
+            return RedirectToAction("Index", "User"); 
         }
 
         public ActionResult About()
