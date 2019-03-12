081b57c2c1fd294968e062480b169d82180326b7
fengchufu <fengchufu@163.com>
2019-03-12 22:45:20 +0800

    update

--- WebApplication/Common/ImageHelp.cs
+++ WebApplication/Common/ImageHelp.cs
@@ -33,7 +33,6 @@ namespace Models
                     }
                     else if (sample.Style == EnumImageStyle.阴文)
                     {
-                        //SolidBrush drawBrush = new SolidBrush(Color.Red);// Create point for upper-left corner of drawing.
                         g.FillRectangle(drawBrush, 0, 0, size, size);
                     }
                 }
@@ -57,24 +56,53 @@ namespace Models
                     }
                     else if (sample.Style == EnumImageStyle.阴文)
                     {
-                        //SolidBrush drawBrush = new SolidBrush(Color.Red);// Create point for upper-left corner of drawing.
                         g.FillEllipse(drawBrush, 0, 0, size, size);
                     }
                 }
             }
-            else if (sample.ImageType == EnumImageType.扁章)
+            else if (sample.ImageType == EnumImageType.扁章) //没有背景图
             {
-
+                imgBack = new System.Drawing.Bitmap(size, size/2);
+                g = Graphics.FromImage(imgBack);
+                if (sample.Style == EnumImageStyle.阳文)
+                {
+                    Pen pen = new Pen(Color.Red, 14.0F);
+                    g.DrawRectangle(pen, 0, 0, size, size/2);
+                }
+                else if (sample.Style == EnumImageStyle.阴文)
+                {
+                    g.FillRectangle(drawBrush, 0, 0, size, size/2);
+                }
             }
-            else if (sample.ImageType == EnumImageType.儿童印章)
+            else if (sample.ImageType == EnumImageType.儿童印章) //必须要有背景图
             {
-
+                if (!string.IsNullOrEmpty(sample.BgImage))
+                {
+                    string bgImgPath = AppDomain.CurrentDomain.BaseDirectory + sample.BgImage;
+                    imgBack = Image.FromFile(bgImgPath);     //相框图片 
+                    g = Graphics.FromImage(imgBack);
+                }
+                else
+                {
+                    return null;
+                }
             }
-            else if (sample.ImageType == EnumImageType.个性签名章)
+            else if (sample.ImageType == EnumImageType.个性签名章)  //纯文字，没有边框，没有背景,
             {
-
+                imgBack = new System.Drawing.Bitmap(size, size / 2);
+                g = Graphics.FromImage(imgBack);
+                if (sample.Style == EnumImageStyle.阳文)
+                {
+                    Pen pen = new Pen(Color.Red, 14.0F);
+                    g.DrawRectangle(pen, 0, 0, size, size / 2);
+                }
+                else if (sample.Style == EnumImageStyle.阴文)
+                {
+                    g.FillRectangle(drawBrush, 0, 0, size, size / 2);
+                }
             }
-            //写主文字
+
+            /////////////////////////////////////////写主文字//////////////////////////////////////////////////////
             if (sample.MainText.Count == sample.MainTextNumber)
             {
                 for (int i = 0; i < sample.MainTextNumber; i++)
@@ -91,11 +119,15 @@ namespace Models
                 DrawSmallText(g, sample.SmallText, sample.Style);
             }
             #region 测试 瞄准线，后面要删掉。
-            SolidBrush drawBrush1 = new SolidBrush(Color.Green);// Create point for upper-left corner of drawing.
-            g.DrawLine(new Pen(drawBrush1), new Point(0, 0), new Point(295, 295));
-            g.DrawLine(new Pen(drawBrush1), new Point(0, 295), new Point(295, 0));
-            g.DrawLine(new Pen(drawBrush1), new Point(147, 0), new Point(147, 295));
-            g.DrawLine(new Pen(drawBrush1), new Point(0, 147), new Point(295, 147));
+            if(ifSample)
+            {
+                SolidBrush drawBrush1 = new SolidBrush(Color.Green);// Create point for upper-left corner of drawing.
+                g.DrawLine(new Pen(drawBrush1), new Point(0, 0), new Point(295, 295));
+                g.DrawLine(new Pen(drawBrush1), new Point(0, 295), new Point(295, 0));
+                g.DrawLine(new Pen(drawBrush1), new Point(147, 0), new Point(147, 295));
+                g.DrawLine(new Pen(drawBrush1), new Point(0, 147), new Point(295, 147));
+            }
+      
             #endregion
 
             GC.Collect();
